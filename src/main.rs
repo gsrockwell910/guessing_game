d40ae5b5a6b6626f2f42c90e2654bb8498387935
Gavin <gsrockwell910@gmail.com>
2025-09-19 10:41:28 -0500

    Add functionality to get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io; // io library to get user input
+
+// Entry point of the program
 fn main() {
-    println!("Hello, world!");
+    // Macro to print a String to the screen
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // Mutable variable to store user input
+    let mut guess = String::new();
+
+    // Handle user input
+    // & indicates a reference
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // Prints String containing user input
+    println!("You guessed: {guess}");
 }
